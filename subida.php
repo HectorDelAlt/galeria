39839dedc8acad785c8df1cbb836ba4bf77612f8
HectorDelAlt <hector.del.alt@gmail.com>
2022-12-01 15:51:52 +0000

    Subida

--- subida.php
+++ subida.php
@@ -1,12 +1,32 @@
+<?php include "includes/connection.php" ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página de Subida</title>
+    <title>Subir Imágen</title>
+    <?php include "includes/navBar.php" ?>
 </head>
 <body>
-    
+    <div class="menu">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a href="index.php">Galeria de Fotos</a>
+            </div>
+            <div>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="listado.php"><span class="glyphicon"></span> Listado</a></li>
+                    <li><a href="listadoAutores.php"><span class="glyphicon"></span> Listado de Autores</a></li>
+                    <li><a href="subida.php"><span class="glyphicon"></span> Subir Imagen</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <br><br>
+    <br><br>
+    <h1>Subir Imágen</h1>
 </body>
+
 </html>
