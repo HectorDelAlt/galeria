6557e9f395450255479dad543c251b6c9b579cf0
Alvaro <alvaroromfra@gmail.com>
2022-12-01 17:10:30 +0000

    imágen

--- subida.php
+++ subida.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Subir Imágen</title>
+    <title>Subir Imagen</title>
     <?php include "includes/navBar.php" ?>
     <style>
         .form_new {
