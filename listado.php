88062c6cdaa9eb60d811dff295da1f9dd5b33812
HectorDelAlt <hector.del.alt@gmail.com>
2022-12-01 19:21:13 +0000

    Cambios Basicos

--- listado.php
+++ listado.php
@@ -1,15 +1,26 @@
 <?php include "includes/connection.php" ?>
 <?php
-$sql = "SELECT * FROM images";
+if(isset($_POST['edit'])){
+    $id = $_POST['edit'];
+    header("Location: modificacion.php");
+} else if (isset($_POST['delete'])) {
+    $id = $_POST['delete'];
+    $sql2 = "DELETE FROM images WHERE id=$id";
+
+    $link->exec($sql2);
+    
+    header("Location: listado.php");
+}
+
+$sql = "SELECT A.name, I.id, file, I.enabled, I.created FROM authors A JOIN images I ON A.id = I.author_id;";
 $result = $link->query($sql);
 
 $result->bindColumn('id', $id);
-$result->bindColumn('author_id', $author_id);
-$result->bindColumn('name', $name);
+$result->bindColumn('name', $authorName);
 $result->bindColumn('file', $file);
 $result->bindColumn('enabled', $enabled);
 $result->bindColumn('created', $created);
-$count = $result->fetchAll();
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -18,7 +29,7 @@ $count = $result->fetchAll();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
+    <title>Listado</title>
     <?php include "includes/navBar.php" ?>
     <?php include "includes/tables.php" ?>
 </head>
@@ -55,7 +66,6 @@ $count = $result->fetchAll();
                 <thead>
                     <tr>
                         <th>Autor</th>
-                        <th>Nombre del Fichero</th>
                         <th>Fichero</th>
                         <th>Activo</th>
                         <th>Creado</th>
@@ -66,32 +76,32 @@ $count = $result->fetchAll();
                 </thead>
                 <tbody>
                     <?php
-                    if (count($count) > 0) {
-                        while ($result->fetch()):
+
+                    while ($result->fetch(PDO::FETCH_BOUND)) :
                     ?>
-                    
-                            <tr>
-                                <td><?= $author_id ?></td>
-                                <td><?= $name ?></td>
-                                <td><?= $file ?></td>
-                                <td style="text-align: center;"><?php
-                                                                if ($enabled == 0 && $id) {
-                                                                    echo "<img src='https://emojitool.com/img/apple/ios-15.4/white-circle-1454.png' width='30px' height='30px'>";
-                                                                } else {
-                                                                    echo "<img src='https://emojitool.com/img/apple/ios-15.4/large-blue-circle-543.png' width='30px' height='30px'>";
-                                                                }
-                                                                ?></td>
-                                <td><?= $created ?></td>
+                        <tr>
+                            <td><?= $authorName ?></td>
+                            <td><?= $file ?></td>
+                            <td style="text-align: center;">
+                                <?php
+                                if ($enabled == 0 && $id) {
+                                    echo "<img src='https://emojitool.com/img/apple/ios-15.4/white-circle-1454.png' width='30px' height='30px'>";
+                                } else {
+                                    echo "<img src='https://emojitool.com/img/apple/ios-15.4/large-blue-circle-543.png' width='30px' height='30px'>";
+                                }
+                                ?></td>
+                            <td><?= $created ?></td>
+                            <form method="Post">
                                 <td style="text-align: center;">
-                                    <form method="POST">
-                                        <a class="delete" title="Delete" data-toggle="tooltip" href="listado.php"><i class="material-icons"><button type="submit" name="delete" value='<?= $id ?>'></button></i></a>
-                                    </form>
+                                    <a class="edit" title="Edit" data-toggle="tooltip"><i class="material-icons"><button type="submit" name="edit" value='<?= $id ?>'></button></i></a>
                                 </td>
-                            </tr>
-                    <?php endwhile;
-                    } else {
-                        echo "<tr><td colspan='7' style='text-align: center;'>No hay se han añadido fotos</td></tr>";
-                    } ?>
+                                <td style="text-align: center;">
+                                    <a class="delete" title="Delete" data-toggle="tooltip"><i class="material-icons"><button type="submit" name="delete" value='<?= $id ?>'></button></i></a>
+                                </td>
+                            </form>
+                            </td>
+                        </tr>
+                    <?php endwhile; ?>
             </table>
         </div>
     </div>
